81934f23a4252e6d906e80a23b5bf4bff3bbc27b
Aamir Nazir Kuchay <aamir.redstaglabs@gmail.com>
2023-02-16 22:55:44 +0530

    product id

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -19,7 +19,16 @@ const ProductCard = (props) => {
           location.pathname === "/product" ? `gr-${grid}` : "col-3"
         }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname === "/"
+              ? "/product/:id"
+              : location.pathname === "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <Link to="#">
               <img src={wish} alt="imaged" />
@@ -73,7 +82,16 @@ const ProductCard = (props) => {
           location.pathname === "/product" ? `gr-${grid}` : "col-3"
         }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname === "/"
+              ? "/product/:id"
+              : location.pathname === "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <Link to="#">
               <img src={wish} alt="imaged" />
